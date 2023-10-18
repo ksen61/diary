7f68503820525aca01372948f1fc9c275696d529
ksen61 <kspankova13@gmail.com>
2023-10-18 19:04:35 +0300

    diary
    test

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using проба;
+
+
+
+DateTime tekdata;
+while (true)
+{
+    tekdata = Menu.date;
+    Console.WriteLine(Menu.date.ToString());
+    foreach (Note i in Menu.za)
+    {
+        if (i.data.Date == tekdata.Date)
+        {
+            Console.WriteLine("  " + i.nazvanie);
+            Menu.zametkiekran.Add(i);
+        }
+    }
+
+    int position = Menu.Show(1, Menu.zametkiekran.Count);
+
+
+
+    if (position == 1)
+    {
+        Console.Clear();
+        Console.WriteLine(Menu.zametkiekran[0].nazvanie);
+        Console.WriteLine(Menu.zametkiekran[0].opisanie);
+        Console.WriteLine("Дата заметки:");
+        Console.WriteLine(Menu.zametkiekran[0].data);
+    }
+    else if (position == 2)
+    {
+        Console.Clear();
+        Console.WriteLine(Menu.zametkiekran[1].nazvanie);
+        Console.WriteLine(Menu.zametkiekran[1].opisanie);
+        Console.WriteLine("Дата заметки:");
+        Console.WriteLine(Menu.zametkiekran[1].data);
+    }
+    Console.SetCursorPosition(0, 6);
+
+    Console.WriteLine("Нажмите Enter, чтобы продолжить...");
+    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+    Console.Clear();
+}
+class Note
+{
+    public string nazvanie;
+    public string opisanie;
+    public DateTime data;
+}
---
+++ menu.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace проба
+{
+    internal class Menu
+    {
+        static Note a = new Note()
+        {
+            nazvanie = "Покушать",
+            opisanie = "Описание: съесть два бутерброда с колбасой",
+            data = new DateTime(2023, 10, 17)
+        };
+        static Note b = new Note()
+        {
+            nazvanie = "Сходить на пары",
+            opisanie = "Описание: с первой по четвертую",
+            data = new DateTime(2023, 10, 17)
+        };
+        static Note s = new Note()
+        {
+            nazvanie = "Убраться дома",
+            opisanie = "Описание: пропылесосить",
+            data = new DateTime(2023, 10, 18)
+        };
+        static Note d = new Note()
+        {
+            nazvanie = "Сделать уроки",
+            opisanie = "Описание: сделать дискретную матемактику и философию",
+            data = new DateTime(2023, 10, 18)
+        };
+        static Note e = new Note()
+        {
+            nazvanie = "Погулять с собакой",
+            opisanie = "Описание: выйти на улицу",
+            data = new DateTime(2023, 10, 19)
+        };
+        static Note f = new Note()
+        {
+            nazvanie = "Приготовить поесть",
+            opisanie = "Описание: сварить макороны",
+            data = new DateTime(2023, 10, 19)
+        };
+        public static Note[] za = { a, b, s, d, e, f };
+        public static List<Note> zametkiekran = new List<Note>();
+        public static DateTime date = DateTime.Now;
+        public static int Show(int minStrelochka, int maxStrelochka)
+        {
+            ConsoleKeyInfo key;
+            int position = 1;
+            date = DateTime.Now;
+
+            do
+            {
+
+                key = Console.ReadKey();
+                Console.SetCursorPosition(0, position);
+                Console.WriteLine("  ");
+                if (key.Key == ConsoleKey.UpArrow && position != minStrelochka)
+                {
+                    position--;
+                }
+                else if (key.Key == ConsoleKey.DownArrow && position != maxStrelochka)
+                {
+                    position++;
+                }
+                ChangeData(key);
+                Console.SetCursorPosition(0, position);
+                Console.WriteLine("->");
+            } while (key.Key != ConsoleKey.Enter);
+            return position;
+        }
+        public static void ChangeData(ConsoleKeyInfo info)
+        {
+            if (info.Key == ConsoleKey.RightArrow)
+            {
+                Menu.zametkiekran.Clear();
+                Console.SetCursorPosition(0, 0);
+                date = date.AddDays(1);
+                Console.Clear();
+                Console.WriteLine(date.ToString());
+                foreach (Note i in Menu.za)
+                {
+                    if (i.data.Date == date.Date)
+                    {
+                        Console.WriteLine("  " + i.nazvanie);
+                        Menu.zametkiekran.Add(i);
+                    }
+                }
+            }
+            else if (info.Key == ConsoleKey.LeftArrow)
+            {
+                Menu.zametkiekran.Clear();
+                Console.SetCursorPosition(0, 0);
+                date = date.AddDays(-1);
+                Console.Clear();
+                Console.WriteLine(date.ToString());
+                foreach (Note i in Menu.za)
+                {
+                    if (i.data.Date == date.Date)
+                    {
+                        Console.WriteLine("  " + i.nazvanie);
+                        Menu.zametkiekran.Add(i);
+                    }
+                }
+            }
+
+
+        }
+    }
+}
